48804e4c41a6f971e1e40ac8023c26997815a328
Thamme Gowda <thammegowda@users.noreply.github.com>
2018-09-19 15:03:26 -0700

    fall back length splitter

--- tools/dataprep/plain2tsv-ssplit.sh
+++ tools/dataprep/plain2tsv-ssplit.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-corenlp-ssplit.scala |  sed 's/^[^[:space:]]\+_\([0-9]\+\)[^[:space:]0-9]\+:\([0-9\.]\+\)\t/\1_\2\t/' 
+# use this script to convert a bunch of plain text files to TSVs. 
+#  1. run coreNLP sentence splitter. this outputs filename.txt:line_number \t sentences
+#  2. convert ugly looking file name to a neat docID_seg_ID using sed
+#  3. If there are any longer sequences than 80 words, split them
+
+corenlp-ssplit.scala | \
+   sed 's/^[^[:space:]]\+_\([0-9]\+\)[^[:space:]0-9]\+:\([0-9\.]\+\)\t/\1_\2\t/' | \
+   ssplit-tsv-len.py -l 80 
 
