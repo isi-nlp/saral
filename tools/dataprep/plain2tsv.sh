e72c212c667c23f364eb350f672164bdd2d8ae3d
Thamme Gowda <tg@isi.edu>
2018-09-21 15:11:51 -0700

    trimming and filtering useless lines

--- tools/dataprep/plain2tsv.sh
+++ tools/dataprep/plain2tsv.sh
@@ -1,6 +1,9 @@
-
+#/usr/bin/env bash
+#this script converts a bunch of .txt files into .tsv file
+# usage: ls *.txt | plain2tsv.sh > out.tsv
+#
 while read f;
 do
-    i=$(echo $f | sed 's/.*_\([0-9]\+\).txt$/\1/');
-    cat -b $f | sed 's/^\s*//' | awk -F '\t' -v i="$i" 'NF >= 2 {print i"_"$1"\t"$2}' ;
+    id=$(echo $f | sed 's/.*_\([0-9]\+\).txt$/\1/');
+    cat $f | sed 's/^\s\+//;s/\s\+$//' | grep -v '^$' | awk -F '\n' -v id="$id" '{printf "%s_%s\t%s\n",id,NR,$0}' ;
 done
