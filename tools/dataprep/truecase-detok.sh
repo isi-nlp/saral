ddc50dc70ceb8363d0226172ab7a83da134b3f78
Thamme Gowda <tg@isi.edu>
2020-05-27 14:47:07 -0700

    detokenizer quiet; update ulftok

--- tools/dataprep/truecase-detok.sh
+++ tools/dataprep/truecase-detok.sh
@@ -2,4 +2,4 @@
 
 detokr=$MOSES/scripts/tokenizer/detokenizer.perl
 [[ -f $detokr ]] || { echo "Cant find MOSES; please export MOSES=/path"; exit 1; }
-corenlp-truecase.scala 2> /dev/null | $detokr -l en -penn
+corenlp-truecase.scala 2> /dev/null | $detokr -l en -penn -q
